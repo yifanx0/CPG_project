3a2c2eecb985ac5bbcb98bc64d9766e4a60962ed
Unknown <oeagen@uchicago.edu>
2019-02-14 13:05:28 -0600

    changed inner join to left join and looped over category

--- code/merge_kilts_sh.R
+++ code/merge_kilts_sh.R
@@ -6,23 +6,26 @@ library(data.table)
 setwd("~/Dropbox/RA2/externals/POG/kilts/")
 
 # choose a category (loop)
-prod_cat = "cer"
-target_dir = paste0(prod_cat, "/")
-zip_name = paste0(target_dir, "w", prod_cat, ".zip")
-temp_dir = tempdir()   # temporary directory to store wxxx.csv
+prod_cats = c("cso", "cer", "ptw", "did", "tpa", "rfj", 
+              "bjc", "tna", "ana", "tti", "ora", "tbr")
+for (prod_cat in prod_cats) {
+  target_dir = paste0(prod_cat, "/")
+  zip_name = paste0(target_dir, "w", prod_cat, ".zip")
+  temp_dir = tempdir()   # temporary directory to store wxxx.csv
+  # unzip move and read in data
+  unzip(zipfile = zip_name, exdir = temp_dir)
+  move_file = paste0(temp_dir, "/w", prod_cat, ".csv")
+  plano_file = paste0(target_dir, "w", prod_cat, "sh.csv")
+  move = fread(move_file)
+  plano = fread(plano_file, drop = c(1))
+  
+  # merge (/clean) dataset and write
+  dt = merge(move, plano, by = c("UPC", "STORE", "WEEK"), 
+             all.x = TRUE, suffixes = c("_m", "_p"))
+  # columns with suffix "_m" come from wxxx (move), "_p" come from wxxxsh (plano)
+  target_file = paste0(target_dir, prod_cat, "_merge.csv.gz")
+  write.csv(dt, file = target_file, row.names = FALSE)
+}
 
-# unzip move and read in data
-unzip(zipfile = zip_name, exdir = temp_dir)
-move_file = paste0(temp_dir, "/w", cat, ".csv")
-plano_file = paste0(target_dir, "w", cat, "sh.csv")
-move = fread(move_file)
-plano = fread(plano_file, drop = c(1))
-
-# merge (/clean) dataset and write
-dt = merge(move, plano, by = c("UPC", "STORE", "WEEK"), suffixes = c("_m", "_p"))
-# columns with suffix "_m" come from wxxx (move), "_p" come from wxxxsh (plano)
-merge_file = paste0(target_dir, "/", prod_cat, "_merge.csv")
-write.csv(dt, file = merge_file, row.names = FALSE)
-
-#c lose zip folder
+# close temp folder
 unlink(temp_dir, recursive = TRUE)
