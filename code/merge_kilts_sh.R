fe37069bb3999c3dbf14e32ee17854f548b77fe7
Unknown <oeagen@uchicago.edu>
2019-02-14 12:25:41 -0600

    finished code for merging planogram with kilts and writing it

--- code/merge_kilts_sh.R
+++ code/merge_kilts_sh.R
@@ -1,20 +1,28 @@
 # Goal: merge movement (kilts) data with planogram data for each category in POG/kilts folder
 # Owen Eagen, Yifan Xu
+# 2/14/2018
 
 library(data.table)
 setwd("~/Dropbox/RA2/externals/POG/kilts/")
 
 # choose a category (loop)
-cat = "ana"
-move_file = paste0("/", cat, "/w", cat, ".csv")
-plano_file = paste0(cat, "/w", cat, "sh.csv")
+prod_cat = "cer"
+target_dir = paste0(prod_cat, "/")
+zip_name = paste0(target_dir, "w", prod_cat, ".zip")
+temp_dir = tempdir()   # temporary directory to store wxxx.csv
 
 # unzip move and read in data
-unzip(zipfile = paste0("/", cat, "/w", cat, ".zip"), 
-      exdir = paste0("~/Dropbox/RA2/externals/POG/kilts/", cat))
+unzip(zipfile = zip_name, exdir = temp_dir)
+move_file = paste0(temp_dir, "/w", cat, ".csv")
+plano_file = paste0(target_dir, "w", cat, "sh.csv")
 move = fread(move_file)
-plano = fread(plano_file)
+plano = fread(plano_file, drop = c(1))
 
 # merge (/clean) dataset and write
+dt = merge(move, plano, by = c("UPC", "STORE", "WEEK"), suffixes = c("_m", "_p"))
+# columns with suffix "_m" come from wxxx (move), "_p" come from wxxxsh (plano)
+merge_file = paste0(target_dir, "/", prod_cat, "_merge.csv")
+write.csv(dt, file = merge_file, row.names = FALSE)
 
 #c lose zip folder
+unlink(temp_dir, recursive = TRUE)
