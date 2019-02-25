eb32121c78617132c8e7e87e9c14034c8373bb90
Unknown <oeagen@uchicago.edu>
2019-02-25 11:21:18 -0600

    fixed merge code and made an R file for running the regression

--- code/merge_kilts_sh.R
+++ code/merge_kilts_sh.R
@@ -7,12 +7,14 @@ setwd("~/Dropbox/RA2/externals/POG/kilts/")
 
 # choose a category (loop)
 prod_cats = c("cso", "cer", "ptw", "did", "tpa", "rfj", 
-              "bjc", "tna", "ana", "tti", "ora", "tbr")
+              "bjc", "tna", "tti", "tbr")
 
 # for these categories, R threw an error when trying to read in the move dataset
 PROBLEM_CATS = c("ana", "ora")
 
+# create merge file for each category and also move upc in
 for (prod_cat in prod_cats) {
+  ## MERGE
   target_dir = paste0(prod_cat, "/")
   zip_name = paste0(target_dir, "w", prod_cat, ".zip")
   temp_dir = tempdir()   # temporary directory to store wxxx.csv
@@ -23,12 +25,19 @@ for (prod_cat in prod_cats) {
   move = fread(move_file)
   plano = fread(plano_file, drop = c(1))
   
-  # merge (/clean) dataset and write
+  # inner merge dataset and write
   dt = merge(move, plano, by = c("UPC", "STORE", "WEEK"), 
-             all.x = TRUE, suffixes = c("_m", "_p"))
-  # columns with suffix "_m" come from wxxx (move), "_p" come from wxxxsh (plano)
-  target_file = paste0(target_dir, prod_cat, "_merge.csv.gz")
+             suffixes = c("_m", "_p"))
+  # NOTE: columns with suffix "_m" come from wxxx (move), "_p" come from wxxxsh (plano)
+  
+  # rhe following two lines are wrong; need to figure out how to write files to zip
+  target_file = paste0(target_dir, prod_cat, "_merge.csv")
   write.csv(dt, file = target_file, row.names = FALSE)
+  
+  ## MOVE UPC IN
+  zip_file_dir = "~/Dropbox/RA2/externals/POG/data_csv/dff_csv.zip"
+  upc_filename = paste0("upc", prod_cat, ".csv")
+  unzip(zip_file_dir, files = c(upc_filename), exdir = target_dir)
 }
 
 # close temp folder
---
+++ code/regression.R
@@ -0,0 +1,6 @@
+# date: 2/25/2019
+# author: Owen Eagen
+# goal: verify consistencies in sales data by regressing shelf characteristics
+
+# sales = a + b1 * price + b2 * up-above-dummy + b3 * bottom-level-dummy + 
+# b4 * disp + b5 * feat + e
